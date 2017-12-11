e52d04cf752a7e026cf55662ea203353fcca99ff
siestaderek <siestaderek.com>
2017-12-10 22:31:23 -0700

    final working

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = require('./server/server');
 //});
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
+app.listen(4000, () => {
+  console.log('Listening');
 });
